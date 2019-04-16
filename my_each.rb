f15093a3bca626238c70ab13932e05c71aae6786
Amy Cen <amy.cen@blackrock.com>
2019-04-16 02:13:43 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  if array.empty?
+    "This block should not run!"
+  else
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+  end
+  array
 end
